3c18c565ef1bc5248ef074f4b039ea501b3fa083
AlexandreFel1pe <alexandre.borges900@gmail.com>
2024-07-05 20:57:54 -0300

    Add entities

---
+++ CleanArchitecture.Domain/Entities/Anime.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Common;
+using CleanArchitecture.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class Anime : BaseEntity
+    {
+        public string? Name { get; set; }
+        public string? Synopsis { get; set; }
+        public float? Score { get; set; }
+        public int? Episodes { get; set; }
+        public string? Studio { get; set; }
+        public AnimeStatus? Status { get; set; }
+        public DateTimeOffset? DateAired { get; set; }
+    }
+}
